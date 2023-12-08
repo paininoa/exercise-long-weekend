96b7e079f4e5cd1b2de026e3e463d704fcca92b6
Alessio <carninoalessio@gmail.com>
2023-12-08 13:50:09 +0100

    Esercizio 3 con Claudio

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,18 @@ Crea un array di oggetti (allLists), in cui ciascun oggetto ha questa struttura:
    type: "ul",
    list: ["Tizio", "Caio]
 }
-Per ogni oggetto, renderizza un div che contiene un titolo con contenuto 'title' e un CustomList con type e list nei props.
+Per ogni oggetto, renderizza un div che contiene un titolo con contenuto 'title' e 
+un CustomList con type e list nei props.
 
 Esercizio 3:
-Sopra a ciascuna lista, aggiungi un div con un input text e un bottone "Aggiungi". Al click di aggiungi, il contenuto dell'input viene aggiunto alla lista.
-Mi raccomando, significa che l'array di oggetti dell'esercizio 2 (allLists) va rifattorizzato dentro ad uno state!
+Sopra a ciascuna lista, aggiungi un div con un input text e un bottone "Aggiungi".
+Al click di aggiungi, il contenuto dell'input viene aggiunto alla lista.
+Mi raccomando, significa che l'array di oggetti dell'esercizio 2 (allLists) 
+va rifattorizzato dentro ad uno state!
 
 Esercizio 4:
-Modifica i props che CustomList accetta. Aggiungi un props 'buttons' che vuole un array di stringhe. Per ogni stringa, crea un bottone su ciascun list item.
+Modifica i props che CustomList accetta. Aggiungi un props 'buttons' che vuole un
+ array di stringhe. Per ogni stringa, crea un bottone su ciascun list item.
 Esempio:
 <CustomList 
    type="ul" 
@@ -35,16 +39,24 @@ Esempio:
 </ul>
 
 Esercizio 5:
-Modifica il tuo componente principale in modo che per ogni CustomList che viene creato, gli vengono passati due bottoni ciascuno: 'Elimina'e 'Modifica'. 
-Al click di Elimina, rimuovi la stringa all'interno dell'array 'list' dell'oggetto corrispondente all'interno dell'array allLists. 
-Al click di Modifica, cambia la stringa all'interno dell'array 'list' dell'oggetto corrispondente all'interno dell'array allLists, con il valore all'interno dell'input text creato ad Esercizio 3.
+Modifica il tuo componente principale in modo che per ogni CustomList che viene creato, 
+gli vengono passati due bottoni ciascuno: 'Elimina'e 'Modifica'. 
+Al click di Elimina, rimuovi la stringa all'interno dell'array 'list' dell'oggetto 
+corrispondente all'interno dell'array allLists. 
+Al click di Modifica, cambia la stringa all'interno dell'array 'list' dell'oggetto 
+corrispondente all'interno dell'array allLists, con il valore all'interno dell'input 
+text creato ad Esercizio 3.
 
 Bonus:
-Crea una React APP in cui l'utente inserisce i dati di un form, attraverso un componente CustomFormElement, che in base al props 'mode' ("input", "select", "checkbox", "radio") ritorna il form element corrispondente (potete usare uno switch, come visto oggi a lezione).
+Crea una React APP in cui l'utente inserisce i dati di un form, attraverso 
+un componente CustomFormElement, che in base al props 'mode' ("input", "select", 
+"checkbox", "radio") ritorna il form element corrispondente (potete usare uno 
+  switch, come visto oggi a lezione).
 Oltre al props 'mode', CustomFormElement deve avere anche i seguenti props: 
 - label: un testo che rappresenta il valore dell'input (es. Your Name)
 - value: il valore del form element
-- onChange: la funzione che viene eseguita all'onChange dell'element, deve venire eseguita passando solo il value non l'intero evento
+- onChange: la funzione che viene eseguita all'onChange dell'element, deve venire 
+eseguita passando solo il value non l'intero evento
 
 */
 }
@@ -53,20 +65,41 @@ import { useState } from "react";
 import "./App.css";
 import CustomList from "./CustomList/CustomList";
 
+const allLists = [
+  {
+    title: "Invitati",
+    type: "ul",
+    list: ["Tizio", "Caio"],
+  },
+  {
+    title: "Partecipanti",
+    type: "ol",
+    list: ["Tizio", "Caio"],
+  },
+  {
+    title: "Altri",
+    type: "ol",
+    list: ["Tizio", "Caio"],
+  },
+];
+
 function App() {
-  const listaProva = ["ciao", "piero"];
-  const allLists = [
-    {
-      title: "Invitati",
-      type: "ul",
-      list: ["Tizio", "Caio"],
-    },
-    {
-      title: "Partecipanti",
-      type: "ol",
-      list: ["Tizio", "Caio"],
-    },
-  ];
+  const [objectList, setObjectList] = useState(allLists);
+  const [inputValues, setInputValues] = useState([]);
+  // const [newItem, setNewItem] = useState(new Array(objectList.length).map(() => ""));
+
+  const handleOnChange = (inputIndex, newValue) => {
+    let newInputValues = [...inputValues];
+    newInputValues[inputIndex] = newValue;
+    setInputValues(newInputValues);
+  };
+
+  const handleAddClick = (sectionIndex) => {
+    let newObjectList = [...objectList];
+    newObjectList[sectionIndex].list.push(inputValues[sectionIndex]);
+    setObjectList(newObjectList);
+    handleOnChange(sectionIndex, "");
+  };
 
   return (
     <>
@@ -74,7 +107,7 @@ function App() {
         07/12 <br />
         exercise-long-weekend
       </h1>
-
+      {/*
       <h2>Esercizio 1</h2>
       <CustomList type="ul" list={listaProva} />
       <CustomList type="ol" list={["ciao", "hey", "piero"]} />
@@ -87,8 +120,23 @@ function App() {
           <CustomList type={e.type} list={e.list} />
         </section>
       ))}
+      */}
 
       <h2>Esercizio 3</h2>
+
+      {objectList.map((e, i) => (
+        <section key={i}>
+          <div>{e.title}</div>
+          <CustomList type={e.type} list={e.list} />
+
+          <input
+            type="text"
+            value={inputValues[i] ?? ""}
+            onChange={(e) => handleOnChange(i, e.target.value)}
+          />
+          <button onClick={() => handleAddClick(i)}>Aggiungi</button>
+        </section>
+      ))}
     </>
   );
 }
